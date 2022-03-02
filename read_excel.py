b798a036c935ee8585b670d34ac7f9e6eb54b1a7
romanzoniit <romanzoniit@gmail.com>
2022-03-02 03:00:43 +0300

    update: parse functions

--- read_excel.py
+++ read_excel.py
@@ -9,7 +9,7 @@ book = openpyxl.open(parsetable.timetable, read_only=True)
 sheet = book.active
 
 def parse_day():
-     dict_l={}
+     dict_l=dict()
      for j in range(sheet.min_column-1, sheet.max_column):
           for i in range(sheet.min_row-1,sheet.max_row):
                if sheet[i][j].value != None and sheet[i][j].value in xlsx_env.dday.keys():
@@ -18,9 +18,9 @@ def parse_day():
                     dict_l[day] = []
                     while sheet[time_idx][j+1].value != xlsx_env.dtime[-1]:
                          if sheet[time_idx][j + 1].value in xlsx_env.dtime:
-                              dict_l[day].append({sheet[time_idx][j+1].value, time_idx})
+                              dict_l[day].append({str(sheet[time_idx][j+1].value).replace(' ', ''): time_idx})
                          time_idx += 1
-                    dict_l[day].append({sheet[time_idx][j + 1].value, time_idx})
+                    dict_l[day].append({str(sheet[time_idx][j+1].value).replace(' ', ''): time_idx})
                     if sheet[time_idx][j + 1].value == xlsx_env.dtime[-1] and day == list(xlsx_env.dday.keys())[-1]:
                          print(dict_l)
                          return dict_l
@@ -36,17 +36,13 @@ def parse_day():
                     for idx, elem in enumerate(data)
                          all_info.append((parse_day()))"""
 
-print(sheet.min_row)
-print(sheet.min_column)
-print(xlsx_env.dday.keys())
-#parse_day()
+
 def parse_group_idx():
-     dict_l = {}
+     dict_l = dict()
      for j in range(sheet.min_column - 1, sheet.max_column):
           for i in range(sheet.min_row - 1, sheet.max_row):
                if sheet[i][j].value == list(xlsx_env.dday.keys())[0] or sheet[i][j].value == xlsx_env.dtime[0]:
                     break
-               print(sheet[i][j].value)
                if sheet[i][j].value != None and sheet[i][j].value in xlsx_env.dgroup:
                     group_idx = j
                     while sheet[i][group_idx].value != xlsx_env.dgroup[-1]:
@@ -56,7 +52,6 @@ def parse_group_idx():
                     dict_l[sheet[i][group_idx].value] = group_idx
                     return dict_l
 
-print(parse_group_idx())
 def parse_time():
      time_list = []
      for row in range(33, 46, 2):
@@ -69,5 +64,6 @@ def parse_groups():
           groups.append(sheet[32][column].value)
      return groups
 
-def parse_num_group(num_group):
-     timetableday=9
+if __name__ == "__main__":
+     print(parse_group_idx())
+     print(parse_day())
